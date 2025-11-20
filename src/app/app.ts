9be37c4e8e4bb1d2a3263804a510a2202b98f25b
mariosb_13 <msanbar104g@iescarrillo.es>
2025-11-20 14:56:53 +0100

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 import { Home } from './components/home/home';
+import { Login } from './components/auth/login/login';
+import { Signin } from './components/auth/signin/signin';
+import { Profile } from './components/dashboard/profile/profile';
+import { Stats } from './components/dashboard/stats/stats';
+import { Taskform } from './components/task/taskform/taskform';
+import { Tasklist } from './components/task/tasklist/tasklist';
+import { Taskresume } from './components/task/taskresume/taskresume';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Footer,Home],
+  imports: [RouterOutlet,Navbar,Footer,Home,Login,Signin,Profile,Stats,Taskform,Tasklist,Taskresume],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
